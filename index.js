15edf5928593946c386c392322057217eb0596b3
Nilay Gupta <nilaygupta1998@gmail.com>
2019-05-17 20:01:00 +0530

    Second commit
    Logging user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const mongoose = require('mongoose');
@@ -11,7 +12,7 @@ mongoose.connect('mongodb://localhost/Logging')
 
 app.use(express.json());
 
-app.post('/api/login', async (req,res) => {
+app.post('/api/register', async (req,res) => {
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -25,27 +26,27 @@ app.post('/api/login', async (req,res) => {
     res.send(user);
 });
 
-// app.post('/api/login', async (req,res) =>{
-//     const {error} = val(req.body);
-//     if (error) return res.status(400).send(error.details[0].message);
+app.post('/api/login', async (req,res) =>{
+    const {error} = val(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-//     let user = await User.findOne({ email: req.body.email });
-//     if (!user) return res.status(400).send('Invalid email or password.');
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
 
-//     const validPassword = await bcrypt.compare(req.body.password, user.password);
-//     if (!validPassword) return res.status(400).send('Invalid email or password.');
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-//     res.send('Login Success');
-// });
+    res.send('Login Success');
+});
 
-// function val(req) {
-//     const schema = {
-//       email: Joi.string().min(5).max(255).required().email(),
-//       password: Joi.string().min(5).max(255).required()
-//     };
+function val(req) {
+    const schema = {
+      email: Joi.string().min(5).max(255).required().email(),
+      password: Joi.string().min(5).max(255).required()
+    };
   
-//     return Joi.validate(req, schema);
-//   }
+    return Joi.validate(req, schema);
+  }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

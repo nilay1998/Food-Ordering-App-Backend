051ca7248bec80f563bff8452352d6e74c2e6d11
Nilay Gupta <nilaygupta1998@gmail.com>
2019-05-18 17:27:28 +0530

    response to json

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const {User, validate} = require('./models/user');
+const bodyParser=require('body-parser');
 
 mongoose.connect('mongodb://localhost/Logging')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/get',async(req,res)=>{
     res.json('RUNNING');
@@ -18,27 +20,28 @@ app.get('/api/get',async(req,res)=>{
 
 app.post('/api/register', async (req,res) => {
     const { error } = validate(req.body); 
-    if (error) return res.status(400).json(error.details[0].message);
+    if (error) return res.json(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).json('User already registered.');
+    if (user) return res.json('User already registered.');
 
     user = new User(_.pick(req.body, ['name', 'email', 'password','phone']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
+
     res.json('Registeration Success');
 });
 
 app.post('/api/login', async (req,res) =>{
     const {error} = val(req.body);
-    if (error) return res.status(400).json(error.details[0].message);
+    if (error) return res.json(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json('Invalid email or password.');
+    if (!user) return res.json('Invalid email or password.');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password.');
+    if (!validPassword) return res.json('Invalid email or password.')
 
     res.json('Login Success');
 });
